7dfaa0d9353a14ce25e19b3a36e739347bc71924
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-09-14 20:28:46 +0200

    Validate and normalize label

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,21 @@
 class Group < ApplicationRecord
+  before_validation :normalize_label
+
+  validates :label, length: { maximum: 100 }
+  validates :label, presence: true
+
+  private
+
+  def normalize_label
+    if self.label
+      self.label = normalize_whitespaces(self.label)
+    end
+  end
+
+  def normalize_whitespaces(string)
+    result = string.gsub(/\s+/, " ")
+    result = result.strip
+    result
+  end
+
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
-RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "Group#label", type: :model do
+
+  context "when it's blank" do
+    it "is invalid" do
+      record = Group.new
+      record.label = ''
+      expect(record).to be_invalid
+      expect(record.errors[:label]).to include("can't be blank")
+    end
+  end
+  context "when it's nil" do
+    it "is invalid" do
+      record = Group.new
+      record.label = nil
+      expect(record).to be_invalid
+      expect(record.errors[:label]).to include("can't be blank")
+    end
+  end
+  context "when it's correct" do
+    it "is valid" do
+      record = Group.new
+      record.label = 'The example group'
+      expect(record).to be_valid
+    end
+  end
+  context "when it's longer than 100 characters" do
+    it "is invalid" do
+      record = Group.new
+      record.label = "a" * 101
+      expect(record).to be_invalid
+      expect(record.errors[:label]).to include("is too long (maximum is 100 characters)")
+    end
+  end
+  context "when it begins and ends with whitespaces characters" do
+    it "is stripped before validation" do
+      record = Group.new
+      record.label = "  \t\n  " + ("a" * 100) + "   \n  \t  "
+      expect(record).to be_valid
+      record.save
+      expect(record.reload.label).to eq ("a" * 100)
+    end
+  end
+
+  context "when it has whitespaces characters between words" do
+    it "they are changed to single spaces before validation" do
+      record = Group.new
+      record.label = "The  \t\n  Example   \n  \t  Label"
+      expect(record).to be_valid
+      record.save
+      expect(record.reload.label).to eq "The Example Label"
+    end
+  end
+
+
 end
