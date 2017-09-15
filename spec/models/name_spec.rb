f21396e6a52cc1dcc3f0fb5df95388bd5539d61d
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-09-15 16:14:18 +0200

    Name without nameset is invalid

--- spec/models/name_spec.rb
+++ spec/models/name_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Name, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Name.new(
+      variants: "Example Name",
+      nameset: Nameset.create!(
+        label: "Example Nameset",
+        source: %(https://en.wikipedia.org/wiki/Category:Greek_masculine_given_names),
+        subgroup: Subgroup.create!(group: Group.create!(label: "The Example Group")))
+    )
+  end
+
+  describe "#nameset" do
+    context "when it's nil" do
+      it "is invalid" do
+        subject.nameset = nil
+        expect(subject).to be_invalid
+        expect(subject.errors[:nameset]).to include("must exist")
+      end
+    end
+    context "when it's correct" do
+      it "is valid" do
+        expect(subject).to be_valid
+      end
+    end
+  end
+  
 end
