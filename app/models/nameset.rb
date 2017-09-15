8ca792f6fa3b40d13a717245fc540b5b895a5845
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-09-15 15:51:04 +0200

    Validate and normalize label

--- app/models/nameset.rb
+++ app/models/nameset.rb
@@ -1,4 +1,24 @@
 class Nameset < ApplicationRecord
   belongs_to :subgroup, counter_cache: true
   acts_as_list scope: :subgroup
+
+  before_validation :normalize_label
+
+  validates :label, length: { maximum: 60 }
+  validates :label, presence: true
+
+  private
+
+  def normalize_label
+    if self.label
+      self.label = normalize_whitespaces(self.label)
+    end
+  end
+
+  def normalize_whitespaces(string)
+    result = string.gsub(/\s+/, " ")
+    result = result.strip
+    result
+  end
+
 end
