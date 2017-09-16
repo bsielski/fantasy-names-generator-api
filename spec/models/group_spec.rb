f250fb18daea55fdd1f11813fe0521efe953cc6e
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-09-16 13:24:47 +0200

    Refactor tests

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,57 +1,59 @@
 require 'rails_helper'
 
-RSpec.describe "Group#label", type: :model do
-
-  context "when it's blank" do
-    it "is invalid" do
-      record = Group.new
-      record.label = ''
-      expect(record).to be_invalid
-      expect(record.errors[:label]).to include("can't be blank")
-    end
-  end
-  context "when it's nil" do
-    it "is invalid" do
-      record = Group.new
-      record.label = nil
-      expect(record).to be_invalid
-      expect(record.errors[:label]).to include("can't be blank")
-    end
-  end
-  context "when it's correct" do
-    it "is valid" do
-      record = Group.new
-      record.label = 'The example group'
-      expect(record).to be_valid
-    end
-  end
-  context "when it's longer than 100 characters" do
-    it "is invalid" do
-      record = Group.new
-      record.label = "a" * 101
-      expect(record).to be_invalid
-      expect(record.errors[:label]).to include("is too long (maximum is 100 characters)")
-    end
-  end
-  context "when it begins and ends with whitespaces characters" do
-    it "is stripped before validation" do
-      record = Group.new
-      record.label = "  \t\n  " + ("a" * 100) + "   \n  \t  "
-      expect(record).to be_valid
-      record.save
-      expect(record.reload.label).to eq ("a" * 100)
-    end
+RSpec.describe "Group", type: :model do
+  subject do
+    Group.new(
+      label: "Example Group",
+      position: 1
+    )
   end
 
-  context "when it has whitespaces characters between words" do
-    it "they are changed to single spaces before validation" do
-      record = Group.new
-      record.label = "The  \t\n  Example   \n  \t  Label"
-      expect(record).to be_valid
-      record.save
-      expect(record.reload.label).to eq "The Example Label"
-    end
-  end
+  describe "#label", type: :model do
 
+    context "when it's blank" do
+      it "is invalid" do
+        subject.label = ''
+        expect(subject).to be_invalid
+        expect(subject.errors[:label]).to include("can't be blank")
+      end
+    end
+    context "when it's nil" do
+      it "is invalid" do
+        subject.label = nil
+        expect(subject).to be_invalid
+        expect(subject.errors[:label]).to include("can't be blank")
+      end
+    end
+    context "when it's correct" do
+      it "is valid" do
+        subject.label = 'The example group'
+        expect(subject).to be_valid
+      end
+    end
+    context "when it's longer than 100 characters" do
+      it "is invalid" do
+        subject.label = "a" * 101
+        expect(subject).to be_invalid
+        expect(subject.errors[:label]).to include("is too long (maximum is 100 characters)")
+      end
+    end
+    context "when it begins and ends with whitespaces characters" do
+      it "is stripped before validation" do
+        subject.label = "  \t\n  " + ("a" * 100) + "   \n  \t  "
+        expect(subject).to be_valid
+        subject.save
+        expect(subject.reload.label).to eq ("a" * 100)
+      end
+    end
+    context "when it has whitespaces characters between words" do
+      it "they are changed to single spaces before validation" do
+        subject.label = "The  \t\n  Example   \n  \t  Label"
+        expect(subject).to be_valid
+        subject.save
+        expect(subject.reload.label).to eq "The Example Label"
+      end
+    end
+
+  end
 
 end
