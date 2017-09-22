aff9c770d02445d3ce1349dbf4ab42388c936b80
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-09-22 20:10:23 +0200

    Generate jsonapi_resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :groups, only: :index
-  
+  namespace :api do
+    namespace :v1 do
+      jsonapi_resources :groups
+      jsonapi_resources :subgroups
+      jsonapi_resources :namesets
+      jsonapi_resources :names
+    end
+end
+
 end
