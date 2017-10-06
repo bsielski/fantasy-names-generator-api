a5c139a61ff72526e1d6ab327faf5ecb0dd3c182
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-10-06 21:44:26 +0200

    Fix subgroups structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,40 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
 @g1 = Group.create!(label: "Traditional Celtic names")
+
 @g1a = Subgroup.create!(group: @g1)
 @n1a1 = Nameset.create!(label: "Scottish male", subgroup: @g1a)
 Name.create!(nameset: @n1a1, variants: "Boisil")
 Name.create!(nameset: @n1a1, variants: "Feargan")
 Name.create!(nameset: @n1a1, variants: "Taran")
-@n1a2 = Nameset.create!(label: "Scottish female", subgroup: @g1a)
-Name.create!(nameset: @n1a2, variants: "Brianag")
-Name.create!(nameset: @n1a2, variants: "Fionnghuala")
-Name.create!(nameset: @n1a2, variants: "Muireall")
+@n1a2 = Nameset.create!(label: "Irish male", subgroup: @g1a)
+
 @g1b = Subgroup.create!(group: @g1)
-@n1a1 = Nameset.create!(label: "Irish male", subgroup: @g1b)
-@n1a2 = Nameset.create!(label: "Irish female", subgroup: @g1b)
+@n1b1 = Nameset.create!(label: "Scottish female", subgroup: @g1b)
+Name.create!(nameset: @n1b1, variants: "Brianag")
+Name.create!(nameset: @n1b1, variants: "Fionnghuala")
+Name.create!(nameset: @n1b1, variants: "Muireall")
+@n1b2 = Nameset.create!(label: "Irish female", subgroup: @g1b)
 
 g2 = Group.create!(label: "Traditional Germanic names")
+
 g2a = Subgroup.create!(group: g2)
 ns2a1 = Nameset.create!(label: "Frankish male", subgroup: g2a)
 Name.create!(nameset: ns2a1, variants: "Einhard")
 Name.create!(nameset: ns2a1, variants: "Nordbert")
-ns2a2 = Nameset.create!(label: "Frankish female", subgroup: g2a)
-# no names
-# no names
+ns2a2 = Nameset.create!(label: "Gothic male", subgroup: g2a)
+Name.create!(nameset: ns2a2, variants: "Ermanaric")
+Name.create!(nameset: ns2a2, variants: "Totila")
+ns2a3 = Nameset.create!(label: "Norse male", subgroup: g2a)
+Name.create!(nameset: ns2a3, variants: "Lodin")
+Name.create!(nameset: ns2a3, variants: "Urm")
+
 g2b = Subgroup.create!(group: g2)
-ns2b1 = Nameset.create!(label: "Gothic male", subgroup: g2b)
-Name.create!(nameset: ns2b1, variants: "Ermanaric")
-Name.create!(nameset: ns2b1, variants: "Totila")
+ns2b1 = Nameset.create!(label: "Frankish female", subgroup: g2b)
+# no names
+# no names
 ns2b2 = Nameset.create!(label: "Gothic female", subgroup: g2b)
 Name.create!(nameset: ns2b2, variants: "Helchen")
 Name.create!(nameset: ns2b2, variants: "Sunigilda")
-g2c = Subgroup.create!(group: g2)
-ns2c1 = Nameset.create!(label: "Norse male", subgroup: g2c)
-Name.create!(nameset: ns2c1, variants: "Lodin")
-Name.create!(nameset: ns2c1, variants: "Urm")
-ns2c2 = Nameset.create!(label: "Norse female", subgroup: g2c)
-Name.create!(nameset: ns2c2, variants: "Asa")
-Name.create!(nameset: ns2c2, variants: "Gunnhild")
+ns2b3 = Nameset.create!(label: "Norse female", subgroup: g2b)
+Name.create!(nameset: ns2b3, variants: "Asa")
+Name.create!(nameset: ns2b3, variants: "Gunnhild")
+
 g3 = Group.create!(label: "Other traditional names")
