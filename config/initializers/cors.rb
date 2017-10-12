5a2f420242b3d9a7a5dd17e5f941cdff9698feba
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-10-13 00:59:33 +0200

    Add asterisc

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', "http://fantasynamesgenerator.testingmagic.ovh/"
+    origins 'localhost:3000', '*'
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
