1d4cc5fced6feebdf02666960fd3ce42dd23ff8c
Bartlomiej (@VM) <b.sielski@yandex.com>
2017-10-27 16:19:37 +0200

    Add filter for jsonapi

--- app/resources/api/v1/nameset_resource.rb
+++ app/resources/api/v1/nameset_resource.rb
@@ -1,4 +1,5 @@
 class Api::V1::NamesetResource < JSONAPI::Resource
   attributes :subgroup_id, :position, :label, :source, :name_separator, :variant_separator, :names_count
+  filter :subgroup_id
   has_many :names
 end
