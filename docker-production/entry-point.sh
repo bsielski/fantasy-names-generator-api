a9becbc0bba97010f067e4baf6f2ef049c7ff06c
Bartłomiej Sielski <b.sielski-webdev@gmail.com>
2018-05-30 21:23:01 +0000

    Delete commented lines

--- docker-production/entry-point.sh
+++ docker-production/entry-point.sh
@@ -12,11 +12,6 @@ if [[ $RAILS_ENV == "production" ]]; then
   bundle exec rails db:create
   bundle exec rails db:migrate
   bundle exec rails db:seed
-  #bundle exec rails assets:precompile
-  #mkdir -p /usr/share/nginx/html
-  #cp -R public/* /usr/share/nginx/html
-  #mkdir -p /etc/nginx/conf.d/
-  #cp docker-production/nginx.conf /etc/nginx/conf.d/default.conf
 fi
 
 exec "$@"
