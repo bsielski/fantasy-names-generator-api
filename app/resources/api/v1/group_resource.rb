b3d275d410db2ef56096f9f2dd91b4c5a8503c66
Bartlomiej Sielski <b.sielski.webdev@gmail.com>
2019-02-24 21:41:15 +0100

    Remove custom from attributes

--- app/resources/api/v1/group_resource.rb
+++ app/resources/api/v1/group_resource.rb
@@ -1,4 +1,4 @@
 class Api::V1::GroupResource < JSONAPI::Resource
-  attributes :label, :position, :subgroups_count, :custom
+  attributes :label, :position, :subgroups_count
   has_many :subgroups
 end
